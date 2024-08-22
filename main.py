3fd5c3c38dee5e672728fea7d39de1a09bcd5856
3m0l3y <allan.emily@gmail.com>
2024-08-22 13:47:13 -0700

    frankenstein code revised

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-with open("books/frankenstein.txt") as f:
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+    word_count(book_text)
 
-    def main():
-        file_contents = f.read()
-        print(file_contents)
-        word_count(file_contents)
+def word_count(string):
+    words = string.split()
+    count = len(words)
+    print(f"Number of words in this book: {count}" )
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
     
-    def word_count(string):
-        words = string.split()
-        count = len(words)
-        print(f"Number of words in this book: {count}" )
-        
-    main()
+main()
     
