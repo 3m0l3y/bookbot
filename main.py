097de1c60b771502afbf5b7a2a9d94517bfa479b
3m0l3y <allan.emily@gmail.com>
2024-08-22 13:40:10 -0700

    frankenstein book count words

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+with open("books/frankenstein.txt") as f:
+
+    def main():
+        file_contents = f.read()
+        print(file_contents)
+        word_count(file_contents)
+    
+    def word_count(string):
+        words = string.split()
+        count = len(words)
+        print(f"Number of words in this book: {count}" )
+        
+    main()
+    
